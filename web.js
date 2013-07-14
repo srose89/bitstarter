978b5aeec2490f746ed8c253cf42769f30431854
Ubuntu <ubuntu@ip-172-31-11-198.us-west-2.compute.internal>
2013-07-14 17:46:33 +0000

    Troubleshot web.js round 1

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync(index.html)
+var buffer = fs.readFileSync('index.html')
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
