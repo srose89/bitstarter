2dc4930b87f1f717534bb9077c93d37c845d4812
Ubuntu <ubuntu@ip-172-31-11-198.us-west-2.compute.internal>
2013-07-14 17:53:38 +0000

    Troubleshoot web.js round 2

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync('index.html')
+var buffer = new Buffer(fs.readFileSync('index.html', 'utf-8'))
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
